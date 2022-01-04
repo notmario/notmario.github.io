451113d77c8cb97a9a0c818b919b098035c5215c
not mario <manmario793@gmail.com>
2022-01-04 15:51:31 +0800

    added other stuff

--- index.js
+++ index.js
@@ -17,3 +17,23 @@ document.getElementById("backFromGamesButton").addEventListener("click", () => {
     document.getElementById("intro").classList.remove("spinOut");
     document.getElementById("intro").classList.add("spinIn");
 });
+
+document.getElementById("otherStuffButton").addEventListener("click", () => {
+    // transition intro and games
+    document.getElementById("intro").classList.remove("spinIn");
+    document.getElementById("intro").classList.add("spinOut");
+    document.getElementById("intro").classList.add("hidden");
+    document.getElementById("otherStuff").classList.remove("hidden");
+    document.getElementById("otherStuff").classList.remove("spinOutNormal");
+    document.getElementById("otherStuff").classList.add("spinInNormal");
+});
+
+document.getElementById("backFromOtherStuffButton").addEventListener("click", () => {
+    // transition intro and games
+    document.getElementById("otherStuff").classList.remove("spinInNormal");
+    document.getElementById("otherStuff").classList.add("spinOutNormal");
+    document.getElementById("otherStuff").classList.add("hidden");
+    document.getElementById("intro").classList.remove("hidden");
+    document.getElementById("intro").classList.remove("spinOut");
+    document.getElementById("intro").classList.add("spinIn");
+});
